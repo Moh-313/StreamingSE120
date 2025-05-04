43f91ae3314448808cd3e55b66f5d5b74c48acbc
Moh-313 <mohlotfy313@gmail.com>
2025-05-04 16:17:12 +0300

    test4

--- MainStreamingPlatform/src/mainstreamingplatform/MainStreamingPlatform.java
+++ MainStreamingPlatform/src/mainstreamingplatform/MainStreamingPlatform.java
@@ -3,7 +3,7 @@ package mainstreamingplatform;
 public class MainStreamingPlatform {
 
     public static void main(String[] args) {
-        
+        User u = new User("Moh", "mohlotfy313@gmail.com", "awooga313");
     }
     
 }
